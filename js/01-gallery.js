c00f1d789c197fd5e74f51aad60f143a05632441
NikaDelva <nikadelva@gmail.com>
2023-03-29 00:14:45 +0300

    task 1 completed (except Escape)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+console.log(galleryEl)
+const makeGalleryItem = ({ preview, original, description } = {}) => {
+    return `
+    <li class='gallery__item'>
+        <a class='gallery__link' href='${original}'>
+         <img src='${preview}' alt='${description}' data-source='${original}' class='gallery__image'> 
+        </a>
+    </li>`   
+}
+const galleryPhotosArr = galleryItems.map(photo => {
+    return makeGalleryItem(photo);
+}).join('')
+
+galleryEl.insertAdjacentHTML('beforeend', galleryPhotosArr)
+
+const galleryImageEl = document.querySelector('.gallery__image');
+console.log(galleryImageEl)
+
+galleryEl.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+    const modalEl = basicLightbox.create(`
+        <img src='${event.target.dataset.source}' width='1400' height='900'>`, {});
+    modalEl.show();
+    document.addEventListener('keydown', onEscapeClose);
+}
+
+function onEscapeClose(event) {
+    if (event.code === 'Escape') {
+        modalEl.close();
+    }
+}
+
+
+
